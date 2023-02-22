ca3c0356a6b76588ab0499c50fee3f7affc33b2a
Maxrutto <ruttomaxmillan@gmail.com>
2023-02-22 05:02:38 -0800

    header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,54 @@
+/**
+ * print_alphabet - prints alphabet in lowercase
+ */
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ */
+void print_alphabet_x10(void);
+
+/**
+ * _islower - checks for lowercase character
+ */
+int _islower(int c);
+
+/**
+ * _isalpha - checks for alphabetic character
+ */
+int _isalpha(int c);
+
+/**
+ * print_sign - prints sign of a number
+ */
+int print_sign(int n);
+
+/**
+ * _abs - prints absolute value of an integer
+ */
+int _abs(int);
+
+/**
+ * print_last_digit - prints the last digit of a number
+ */
+int print_last_digit(int);
+
+/**
+ * jack_bauer - Prints every minute of Jack Bauer's day
+ */
+void jack_bauer(void);
+
+/**
+ * times_table - Prints the nine times table
+ */
+void times_table(void);
+
+/**
+ * add - adds two integers
+ */
+int add(int, int);
+
+/**
+ * print_to_98 - prints all natural numbers 
+ */
+void print_to_98(int n);
