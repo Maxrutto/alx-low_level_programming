cd2d2130f86e3ef96dd8f32d98459e0f3d3d2028
Maxrutto <ruttomaxmillan@gmail.com>
2023-02-28 15:06:14 -0800

    main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,59 @@
+/**
+ * _putchar - writes the character c to stdout
+ */
+int _putchar(char c);
+
+/**
+ * reset_to_98 - takes pointer to an int as parameter and updates the value it points to to 98
+ */
+void reset_to_98(int *n);
+
+/**
+ * swap_int - swaps the values of two integers
+ */
+void swap_int(int *a, int *b);
+
+/**
+ * _strlen - returns the length of a string
+ */
+int _strlen(char *s);
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ */
+void _puts(char *str);
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ */
+void print_rev(char *s);
+
+/**
+ * rev_string - reverses a string
+ */
+void rev_string(char *s);
+
+/**
+ * puts2 - prints every other character of a string, starting with the first character
+ */
+void puts2(char *str);
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ */
+void puts_half(char *str);
+
+/**
+ * print_array - prints n elements of an array of integers, followed by a new line
+ */
+void print_array(int *a, int n);
+
+/**
+ * *_strcpy -  copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest
+ */
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _atoi - convert a string to an integer
+ */
+int _atoi(char *s);
