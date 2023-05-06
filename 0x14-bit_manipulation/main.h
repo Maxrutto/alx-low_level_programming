8574182097773750af689d42e1f93e2a9f3129ea
Maxrutto <ruttomaxmillan@gmail.com>
2023-05-06 06:32:29 -0700

    main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ */
+unsigned int binary_to_uint(const char *b);
+
+/**
+ * print_binary - prints the binary representation of a number
+ */
+void print_binary(unsigned long int n);
+
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
