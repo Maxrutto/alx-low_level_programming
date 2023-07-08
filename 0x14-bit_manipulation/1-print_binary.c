53fa0c6c4721c8a9a1e4054c028958c51aab6c05
Maxrutto <ruttomaxmillan@gmail.com>
2023-07-08 01:35:57 -0700

    NEw function

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,41 +1,20 @@
 #include "main.h"
 
 /**
- * print_binary - converts a decimal number to binary
- * and prints the result
- * @n: The decimal number to convert
+ * print_binary - prints the binary representation
+ * of a decimal
+ * @n: unsigned long int.
  */
 void print_binary(unsigned long int n)
 {
-	int bin, placevalue;
-
-	bin = 0;
-
-	placevalue = 1;
-
-	while (n > 0)
+	if (n >> 0)
 	{
-		int rem;
-
-		rem = n & 1;
-		bin += rem * placevalue;
-		n = (n - rem) >> 1;
-		placevalue *= 10;
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
-
-	if (bin == 0)
-		_putchar('0');
 	else
 	{
-		while (bin > 0)
-		{
-			int digit;
-
-			digit = bin & 1;
-			_putchar(digit + '0');
-			bin = (bin - digit) >> 1;
-		}
+		_putchar('0');
 	}
-
-	_putchar('\n');
 }
