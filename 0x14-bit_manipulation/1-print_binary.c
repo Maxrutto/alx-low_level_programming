96b1a14894cbc20cf37a70ac26d20a3a9d221858
Maxrutto <ruttomaxmillan@gmail.com>
2023-08-04 04:08:32 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,13 +9,16 @@ void print_binary(unsigned long int n)
 {
 	int i, mask, bits = sizeof(n) * 8;
 
+	int first_one_found = 0;
+
 	for (i = bits - 1; i >=0; i--)
 	{
 		mask = 1UL << i;
 
-		if ((n & mask) != 0)
-			_putchar('1');
-		else
-			_putchar('0');
+		if ((n & mask) != 0 || first_one_found)
+		{
+			_putchar((n & mask) ? '1' : '0');
+			first_one_found = 1;
+		}
 	}
 }
