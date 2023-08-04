70710b641071d42883b23585b07c06bd9239712f
Maxrutto <ruttomaxmillan@gmail.com>
2023-08-04 13:23:21 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,20 @@
 #include "main.h"
 
 /**
- * print_binary - Prints binary rep of a number
- * @n: The unsigned long integer being converted
- * Return: VOID.
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
  */
 void print_binary(unsigned long int n)
 {
-	int i, mask, bits = sizeof(n) * 8;
-
-	int first_one_found = 0;
-
-	for (i = bits - 1; i >= 0; i--)
+	if (n >> 0)
 	{
-		mask = 1UL << i;
-
-		if ((n & mask) != 0 || first_one_found)
-		{
-			_putchar((n & mask) ? '1' : '0');
-			first_one_found = 1;
-		}
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
-	if (!first_one_found)
+	else
 		_putchar('0');
 }
