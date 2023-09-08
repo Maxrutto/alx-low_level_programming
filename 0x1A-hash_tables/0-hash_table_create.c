1985c0b0a4ebabce15790bc6a19a6b4d82941910
Maxrutto <ruttomaxmillan@gmail.com>
2023-09-08 13:41:47 -0700

    HashTable create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,7 +2,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_t - Function that creates a hash table
+ * hash_table_create - Function that creates a hash table
  * @size: The size of the HashTable
  *
  * Return: A pointer to the newly created hash table
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	table->size = size;
-	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t));
+	table->array = (hash_node_t **)malloc(sizeof(hash_node_t) * size);
 	if (table->array == NULL)
 	{
 		perror("malloc failure");
