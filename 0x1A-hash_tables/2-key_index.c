001b68064a1e1e51672f1fcc8711874e9cdcef5d
Maxrutto <ruttomaxmillan@gmail.com>
2023-09-08 15:03:28 -0700

    key->index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+/**
+ * key_index - function that generates an index
+ * according to a given hash
+ *
+ * @key: key passed
+ * @size: sizeof the hash tables
+ * Return: key index
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	if (key == NULL || size == 0)
 		return (0);
 	return (hash_djb2(key) % size);
+}
